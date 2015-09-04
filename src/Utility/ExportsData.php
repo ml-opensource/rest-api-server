214fb666e3a528bb7d5d44fd014bf8c7df4df943
Simon Yousoufov <syousoufov@fuzzproductions.com>
2015-09-04 09:57:39 -0400

    Escape commas and double quotes

--- src/Utility/ExportsData.php
+++ src/Utility/ExportsData.php
@@ -60,9 +60,9 @@ trait ExportsData
 						$location = $location[$step];
 					}
 
-					$row_data[$header] = $location;
+					$row_data[$header] = '"' . $location . '"';
 				} else {
-					$row_data[$header] = $row[$column];
+					$row_data[$header] = '"' . $row[$column] . '"';
 				}
 			}
 
