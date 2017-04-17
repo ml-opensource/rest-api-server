5a881ba9c53b8368ac6eef92ea7339a3c379ed58
Kirill Fuchs <kfuchs@fuzzproductions.com>
2017-04-17 16:38:06 -0400

    updated config

--- src/config/api.php
+++ src/config/api.php
@@ -1,17 +1,19 @@
 <?php
 
 return [
-	/*
-	|--------------------------------------------------------------------------
-	| Responder Configuration
-	|--------------------------------------------------------------------------
-	|
-	| Maps formats to API Responders
-	|
+    /*
+    |--------------------------------------------------------------------------
+    | Responder Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Maps formats to API Responders
+    |
     */
-	'responders' => [
-		'json' => \Fuzz\ApiServer\Response\JsonResponder::class,
-		'csv'  => \Fuzz\ApiServer\Response\CsvResponder::class,
-		'xml'  => \Fuzz\ApiServer\Response\XMLResponder::class,
-	],
+    'responders' => [
+        'json' => \Fuzz\ApiServer\Response\JsonResponder::class,
+        'csv' => \Fuzz\ApiServer\Response\CsvResponder::class,
+        'xml' => \Fuzz\ApiServer\Response\XMLResponder::class,
+        'xls' => \Fuzz\ApiServer\Response\XLSResponder::class,
+        'xlxs' => \Fuzz\ApiServer\Response\XLSXResponder::class,
+    ],
 ];
