8440471dc1bb4a70981e771c8b69e9dad10a7f70
Simon Yousoufov <syousoufov@fuzzproductions.com>
2017-09-14 19:01:27 -0400

    TokenThrottler should throttle only by token and not route

--- src/Throttling/TokenThrottler.php
+++ src/Throttling/TokenThrottler.php
@@ -5,7 +5,7 @@ namespace Fuzz\ApiServer\Throttling;
 use Closure;
 use Fuzz\ApiServer\Throttling\Contracts\Throttler;
 use Illuminate\Http\Request;
-use LucaDegasperi\OAuth2Server\Facades\Authorizer;
+use League\OAuth2\Server\Exception\InvalidRequestException;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -23,6 +23,7 @@ class TokenThrottler extends ArbitraryStringThrottler implements Throttler
 	 * @const string
 	 */
 	const THROTTLE_TYPE = 'token';
+	const TOKEN_HEADER  = 'Authorization';
 
 	/**
 	 * Throttle a request
@@ -31,15 +32,15 @@ class TokenThrottler extends ArbitraryStringThrottler implements Throttler
 	 * @param \Closure                 $next
 	 * @param int                      $max_attempts
 	 * @param int                      $decay_minutes
+	 * @param string                   $token_header
 	 *
 	 * @return \Symfony\Component\HttpFoundation\Response
+	 * @throws \League\OAuth2\Server\Exception\InvalidRequestException
 	 */
-	public function handle(Request $request, Closure $next, int $max_attempts = 60, int $decay_minutes = 1): Response
+	public function handle(Request $request, Closure $next, int $max_attempts = 60, int $decay_minutes = 1, string $token_header = self::TOKEN_HEADER): Response
 	{
 		$key = implode(':', [
-			$request->getRequestUri(),
-			$request->method(),
-			Authorizer::getAccessToken(), // Has a __toString()
+			$this->getTokenFromRequest($request, $token_header),
 		]);
 
 		$headers = self::assertThrottle($key, $max_attempts, $decay_minutes);
@@ -49,5 +50,23 @@ class TokenThrottler extends ArbitraryStringThrottler implements Throttler
 		$response->headers->add($headers);
 
 		return $response;
+	}
+
+	/**
+	 * @param \Illuminate\Http\Request $request
+	 * @param string                   $token_header
+	 *
+	 * @return mixed
+	 * @throws \League\OAuth2\Server\Exception\InvalidRequestException
+	 */
+	protected function getTokenFromRequest(Request $request, string $token_header): string
+	{
+		$token = $request->headers->get($token_header, null);
+
+		if (is_null($token)) {
+			throw new InvalidRequestException('access token');
+		}
+
+		return $token;
 	}
 }
